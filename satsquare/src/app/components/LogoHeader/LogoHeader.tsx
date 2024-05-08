0be3f03847733d282d7c4ead99dcb2da16123ad0
Ismail MOUYAHADA <access-energies@tech-center.com>
2024-05-08 19:03:53 +0200

    fix

---
+++ satsquare/src/app/components/LogoHeader/LogoHeader.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image from 'next/image';
+
+function LogoHeader() {
+  return (
+    <>
+    <Image src={"/imgs/logo-header.png"} alt="alt" width={353} height={94} />
+    </>
+  )
+}
+
+export default LogoHeader
