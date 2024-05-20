efe972e3cb8aa5510eec6f40fde2bfa8786881ad
Ismail MOUYAHADA <ismail.mouyahada@gmail.com>
2024-05-20 19:27:26 +0200

    fix page header

--- satsquare/src/components/Sponsor/SponsorSearchBar.tsx
+++ satsquare/src/components/Sponsor/SponsorSearchBar.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react';
+import { FaPlus, FaSearch } from 'react-icons/fa';
 
 interface SponsorSearchBarProps {
   onAdd: () => void;
@@ -17,17 +18,19 @@ const SponsorSearchBar: FC<SponsorSearchBarProps> = ({ onAdd, onSearch }) => {
   };
 
   return (
-    <div className="flex items-center space-x-2 mb-4">
-      <button className="bg-[#F8D99B] text-white py-2 px-4 rounded-md" onClick={onAdd}>Ajouter un nouveau</button>
-      <input 
-        type="text" 
-        placeholder="Chercher un élément ..." 
-        className="border p-2 rounded-md flex-grow bg-[#EEEEEF]" 
-        value={searchTerm} 
-        onChange={handleSearchChange} 
+    <div className="flex justify-between mb-4 space-x-2">
+    <button className="bg-[#EEEEEF] w-1/6 shadow-md text-[#6D6B81] font bold py-2 pr-4 rounded-md flex flex-row items-center justify-center" onClick={onAdd}  > <span className='p-1.5 mx-2 rounded-full bg-slate-400'><FaPlus className='text-white' /></span> <span className='text-[#737ABA] font-bold mx-3'> Ajouter un nouveau</span></button>
+    <div className='bg-[#EEEEEF] w-1/2 flex  p-1.3  pl-2 rounded-md shadow-md '>
+      <input
+        type="text"
+        placeholder="Chercher un événement ..."
+        className="border-none p-2 rounded-md  w-full bg-[#EEEEEF]"
+        value={searchTerm}
+        onChange={handleSearchChange}
       />
-      <button className="bg-[#F8D99B] p-2 rounded-md" onClick={handleSearchClick}>🔍</button>
+      <button className="bg-[#F8D99B] w-auto  p-3.5 rounded-md" onClick={handleSearchClick}> <FaSearch className='text-[#6D6B81] scale-125' /></button>
     </div>
+  </div>
   );
 };
 
