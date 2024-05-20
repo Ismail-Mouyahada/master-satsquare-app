9ba67c4c6d98a376384936f6b45bebd482813e98
Ismail MOUYAHADA <ismail.mouyahada@gmail.com>
2024-05-20 17:54:31 +0200

    fix bug

--- satsquare/src/components/Sidebar/page.tsx
+++ satsquare/src/components/Sidebar/page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { FaCalendarAlt, FaUsers, FaFileAlt, FaChartBar, FaHeart, FaLeaf, FaShieldAlt, FaSignOutAlt, FaBtc, FaCogs, FaEvernote, FaDonate, FaBuilding, FaUserAlt, FaGooglePlusG, FaAccessibleIcon, FaPlug, FaLightbulb, FaGamepad } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { FaCalendarAlt, FaUsers, FaFileAlt, FaChartBar, FaHeart, FaShieldAlt, FaSignOutAlt, FaBtc, FaCogs, FaDonate, FaBuilding, FaUserAlt, FaLightbulb, FaGamepad, FaBars } from 'react-icons/fa';
 import styles from './Sidebar.module.css';
 import Link from 'next/link';
 import { Tooltip } from "@nextui-org/tooltip";
@@ -13,25 +13,34 @@ const links = [
     { href: "/associations", icon: <FaBuilding />, tooltip: "Associations" },
     { href: "/donations", icon: <FaDonate />, tooltip: "Donations" },
     { href: "/evenements", icon: <FaCalendarAlt />, tooltip: "Evenements" },
-    { href: "/profile", icon: <FaUserAlt />, tooltip: "profile" },
+    { href: "/profile", icon: <FaUserAlt />, tooltip: "Profile" },
     { href: "/lightning", icon: <FaLightbulb />, tooltip: "Lightning" },
-    { href: "/configurations", icon: <FaCogs />, tooltip: "Profile" },
+    { href: "/configurations", icon: <FaCogs />, tooltip: "Configurations" },
 ];
 
 const Sidebar: React.FC = () => {
-    return (
-        <div className={styles.sidebar}>
-            <div className='flex items-center justify-center w-full h-16 bg-slate-800'  ><FaBtc className=' scale-[2]' /></div>
-            <div className={styles.main}>
-                {links.map((link, index) => (
-                    <Tooltip key={index} showArrow={true}  placement='right' className='px-2 mr-1 rounded-md bg-[#352B5B]' content={link.tooltip}>
-                        <Link href={link.href} className={styles.iconMiddle}>{link.icon}</Link>
-                    </Tooltip>
-                ))}
-            </div>
-            <div className='flex items-center justify-center w-10 h-10 text-red-500 bg-opacity-75 rounded-full hover:bg-opacity-90 bg-slate-100'  ><FaSignOutAlt className=' scale-[1.2]' /></div>
+    const [isOpen, setIsOpen] = useState(false);
 
-        </div>
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <div className={styles.sidebar}>
+                <div className={`${styles.sidebar} ${isOpen ? 'open' : ''}`}>
+                    <div className='flex items-center justify-center w-full h-16 bg-[#12095c]'  ><FaBtc className=' scale-[2]' /></div>
+                    <div className={styles.main}>
+                        {links.map((link, index) => (
+                            <Tooltip key={index} showArrow={true} placement='right' className='px-2 mr-1 rounded-md bg-[#352B5B]' content={link.tooltip}>
+                                <Link href={link.href} className={styles.iconMiddle}>{link.icon}</Link>
+                            </Tooltip>
+                        ))}
+                    </div>
+                    <div className='flex items-center justify-center w-10 h-10 text-red-500 bg-opacity-75 rounded-full hover:bg-opacity-90 bg-slate-100'  ><FaSignOutAlt className=' scale-[1.2]' /></div>
+                </div>
+            </div>
+        </>
     );
 };
 
